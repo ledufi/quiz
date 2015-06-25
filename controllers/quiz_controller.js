b6101eca24d1811acae81fefc32b01e8ebac1b9f
unknown <Efigueras@BLAAT.348servicios.com>
2015-06-25 09:33:23 +0100

    primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,15 @@
+exports.question = function (req,res){
+	res.render('quizes/question',{ pregunta : 'capital de Italia'});
+};
+
+exports.answer = function (req,res){
+	if ( req.query.respuesta === 'Roma'){
+		res.render('quizes/answer',{ respuesta : 'correcto'});		
+	}
+	else{
+		res.render('quizes/answer',{ respuesta : 'incorrecto'});	
+	}
+};
+
+
+
