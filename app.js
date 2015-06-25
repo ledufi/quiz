99e26df93f07f4739e3c3639799c7de3a79d1719
unknown <Efigueras@BLAAT.348servicios.com>
2015-06-25 11:48:09 +0100

    marco de la aplicación

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-
+var partials = require ('express-partials');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use ( partials() );
 app.use('/', routes);
 
 
