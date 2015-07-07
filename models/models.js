99bd8bd44b3731219d92c83180e7b628dd51c926
unknown <efigueras@BLAAT.348servicios.com>
2015-07-07 09:58:02 +0100

    Despliegue en heroku con sequelize

--- models/models.js
+++ models/models.js
@@ -1,8 +1,35 @@
 var path = require ('path');
 var Sequelize = require ('sequelize');
+var env = require ( '../env');
 
-var sequelize = new Sequelize ( null, null, null, 
-							{dialect:"sqlite", storage:"quiz.sqlite"});
+if (  !process.env.DATABASE_URL ){
+	process.env.DATABASE_URL = env.DATABASE_URL;
+	process.env.DATABASE_STORAGE = env.DATABASE_STORAGE;
+}
+
+var url = process.env.DATABASE_URL.match(/(.*):\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = ( url[6] || null);
+var user = ( url[2] || null);
+var pwd = ( url[3] || null);
+var protocol = ( url[1] || null);
+var dialect = ( url[1] || null);
+var port = ( url[5] || null);
+var host = ( url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
+console.log ( JSON.stringify(url) );
+var sequelize = new Sequelize ( DB_name, user, pwd , 
+	{ dialect : protocol,
+	protocol:protocol,
+	port: port,
+	host: host,
+	storage: storage,
+	omitNull : true
+	}
+);
+
+//var sequelize = new Sequelize ( null, null, null, 
+//							{dialect:"sqlite", storage:"quiz.sqlite"});
 
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
